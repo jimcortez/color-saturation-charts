ed7391bf16ce4bf910f1411efdd05a64c8af1a76
Jim Cortez <jim@jimcortez.com>
2024-10-08 16:57:01 -0700

    add custom color picker

--- js/app.js
+++ js/app.js
@@ -3,6 +3,7 @@ import 'reveal.js/dist/reveal.css'
 import 'reveal.js/dist/theme/white.css'
 import spectral from 'spectral.js'
 import tinycolor from "tinycolor2";
+import jscolor from '@eastdesire/jscolor';
 
 console.log(Reveal)
 console.log(spectral)
@@ -29,32 +30,58 @@ function create_saturation_row(title, values){
     return row_template.content;
 }
 
-function create_saturation_slide(colorConfig) {
+function _create_section_node(title, extras){
     const template = document.createElement('template');
     template.innerHTML = `
         <section data-auto-animate>
-            <h4 data-id="title">Saturation Chart - ${colorConfig.display_name}</h4>
+            <h4 class="slide-title" data-id="title">${title}</h4>
+            <div class="color_rows"></div>
+            ${extras || ""}
         </section>`
-    let slide_template = template.cloneNode(true)
+    return template
+}
 
-    const steps = 15
-    const mainColor = tinycolor(colorConfig.color).setAlpha(1)
-    mainColor.setAlpha(.99)
-    const mainColorRBG = mainColor.toRgbString()
+function create_custom_saturation_slide(colorConfig){
+    const configColor = tinycolor(colorConfig.color)
 
-    let rows = [
-        create_saturation_row('Dilution', spectral.palette(mainColorRBG, mainColor.clone().setAlpha(0).toRgbString(), steps, spectral.RGBA)),
-        create_saturation_row('Tinting', spectral.palette(mainColorRBG, tinycolor("white").toRgbString(), steps)),
-        create_saturation_row('Toning', spectral.palette(mainColorRBG, tinycolor("grey").toRgbString(), steps)),
-        create_saturation_row('Shading', spectral.palette(mainColorRBG, tinycolor("black").toRgbString(), steps)),
-        create_saturation_row('Value', spectral.palette(mainColorRBG, mainColor.complement().toRgbString(), steps)),
-        create_saturation_row('Mix', spectral.palette(mainColorRBG, mainColor.greyscale().toRgbString(), 2)),
-    ]
+    const slide_template = _create_section_node(`Saturation Chart`, `<input id="custom_color_picker" data-id="picker" data-jscolor="{}" value="${configColor.toRgbString()}" />`)
+    const picker = new JSColor(slide_template.content.firstElementChild.querySelector('#custom_color_picker'), {
+        preset: "dark large thick",
+        format: 'rgba'
+    })
 
-    rows.forEach(row => slide_template.content.firstElementChild.appendChild(row))
 
+    var slide_template_node = Array.prototype.slice.call(slide_template.content.childNodes)[0].nextElementSibling;
     
-    return slide_template.content;
+    picker.onChange = ()=> {
+        const mainColor = tinycolor(picker.toRGBAString())
+        const mainColorRBG = mainColor.toRgbString()
+
+        let name = mainColor.toName() || mainColor.toRgbString();
+        if(configColor.toRgbString() === mainColorRBG){
+            name = colorConfig.display_name
+        }
+        
+        slide_template_node.querySelector('.slide-title').innerHTML = `Saturation Chart - ${name}`
+        slide_template_node.querySelector('.color_rows').innerHTML = '';
+
+        const steps = 15
+    
+        let rows = [
+            create_saturation_row('Dilution', spectral.palette(mainColorRBG, mainColor.clone().setAlpha(0).toRgbString(), steps, spectral.RGBA)),
+            create_saturation_row('Tinting', spectral.palette(mainColorRBG, tinycolor("white").toRgbString(), steps)),
+            create_saturation_row('Toning', spectral.palette(mainColorRBG, tinycolor("grey").toRgbString(), steps)),
+            create_saturation_row('Shading', spectral.palette(mainColorRBG, tinycolor("black").toRgbString(), steps)),
+            create_saturation_row('Value', spectral.palette(mainColorRBG, mainColor.complement().toRgbString(), steps)),
+            create_saturation_row('Mix', spectral.palette(mainColorRBG, mainColor.greyscale().toRgbString(), 2)),
+        ]
+    
+        rows.forEach(row => slide_template_node.querySelector('.color_rows').appendChild(row))
+    }
+
+    picker.onChange()
+
+    return slide_template.content
 }
 
 let slideElem = document.getElementById('slides')
@@ -74,12 +101,7 @@ const colors = [
     {display_name: "Yellow-Orange", color: spectral.mix(tinycolor("yellow").toRgbString(), tinycolor("orange").toRgbString(), 0.5)},
 ]
 
-colors.forEach(color => slideElem.appendChild(create_saturation_slide(color)))
+colors.forEach(color => slideElem.appendChild(create_custom_saturation_slide(color)))
 
-window.requestAnimationFrame(() => {
-    Reveal.initialize({
-    });
-    setTimeout(()=>Reveal.layout())
-    
-});
-  
+
+Reveal.initialize();
